c25f0e423da9d822da3a60b85a0afef68d037b3b
marksikaundi <emmanuelmark117@gmail.com>
2023-04-04 19:04:30 +0200

    examples

---
+++ Week 1/Examples/example.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <p>This is the sample JSX</p>
+      <ul>
+        <li>List No. 1</li>
+        <li>List No. 2</li>
+      </ul>
+    </div>
+  );
+}
