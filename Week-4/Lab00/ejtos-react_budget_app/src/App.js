4937ae04f94f5983eb21e0f4d20fc0b0082f9d5d
marksikaundi <emmanuelmark117@gmail.com>
2023-04-15 23:04:09 +0200

    labs

---
+++ Week-4/Lab00/ejtos-react_budget_app/src/App.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
+//Code to import Budget.js
+import Budget from "./components/Budget";
+
+// Add code to import the other components here under
+
+import { AppProvider } from "./context/AppContext";
+import Remaining from "./components/Remaining";
+const App = () => {
+  return (
+    <AppProvider>
+      <div className="container">
+        <h1 className="mt-3">Company's Budget Allocation</h1>
+        <div className="row mt-3">
+              {/* Add Budget component here under */}
+          <div className="col-sm">
+            <Budget />
+          </div>
+              {/* Add Remaining component here under */}   {" "}
+          <div className='col-sm'>
+                        <Remaining />
+                    </div>
+          {/* Add ExpenseTotal component here under */}   {" "}
+          <div className='col-sm'>
+                        <ExpenseTotal />
+                    </div>
+          {/* Add ExpenseList component here under */}   {" "}
+          {/* Add ExpenseItem component here under */}   {" "}
+          {/* Add AllocationForm component here under */}
+        </div>
+      </div>
+    </AppProvider>
+  );
+};
+export default App;
